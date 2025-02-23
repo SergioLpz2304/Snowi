965246a7ca8b9e258df8e568c0dc305d886df894
Sergio López echeverry <Sergio>
2025-02-22 20:48:22 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SnowMap from "./SnowMap";
+
+function App() {
+  return (
+    <div>
+      <h1>Snowi - Snow Tracking App</h1>
+      <SnowMap />
+    </div>
+  );
+}
+
+export default App;
